d7688cf950e68d6df2bc26e9541d84d89ddab8e2
yeunganty <p7v9a@mail.ubc.ca>
2020-04-04 18:26:09 -0700

    Added in new database start for demo

--- src/main/resources/db/migration/V0___seniorServe.sql
+++ src/main/resources/db/migration/V0___seniorServe.sql
@@ -1,3 +1,62 @@
+CREATE TABLE PostalCode (
+	PostalCode 		CHAR(6),
+	City			VARCHAR(20),
+	Province		CHAR(2),
+	PRIMARY KEY (PostalCode));
+
+INSERT INTO PostalCode (PostalCode, City, Province)
+VALUES
+('M5S1A1', 'Toronto', 'ON'),
+('V6T1Z4', 'Vancouver', 'BC'),
+('T2N1N4', 'Calgary', 'AB'),
+('T6G2R3', 'Edmonton', 'AB'),
+('N2L3G1', 'Waterloo', 'ON'),
+('G1V0A6', 'Quebec', 'QC'),
+('V5A1S6', 'Burnaby', 'BC'),
+('N6A3K7', 'London', 'ON');
+
+CREATE TABLE Location (
+	PostalCode		CHAR(6),
+	Address		VARCHAR(50),
+	PRIMARY KEY (PostalCode, Address),
+	FOREIGN KEY (PostalCode) REFERENCES PostalCode
+		On update cascade
+		On delete no action);
+
+INSERT INTO Location (PostalCode, Address)
+VALUES
+('M5S1A1', '27 King''s College Circle'),
+('V6T1Z4', '2329 West Mall'),
+('T2N1N4', '2500 University Dr'),
+('T6G2R3', '116 St & 85 Ave'),
+('N2L3G1', '200 University Ave W'),
+('G1V0A6', '2325 Rue de l''Universite'),
+('V5A1S6', '8888 University Dr'),
+('N6A3K7', '1151 Richmond St');
+
+CREATE TABLE Users (
+    Username VARCHAR(50) NOT NULL PRIMARY KEY,
+    First_Name VARCHAR(100) NOT NULL,
+    Last_Name VARCHAR(100),
+    PostalCode CHAR(6),
+    Address VARCHAR(50),
+    FOREIGN KEY (PostalCode, Address) REFERENCES Location
+        on delete set NULL
+);
+
+INSERT INTO Users (Username, First_Name, Last_Name, PostalCode, Address)
+VALUES
+('Sarah123', 'Sarah', 'Stevens', 'G1V0A6', '2325 Rue de l''Universite'),
+('Dorothy45', 'Dorothy', 'Davidson', 'N2L3G1',  '200 University Ave W'),
+('Ann34', 'Ann', 'Yeung', 'T6G2R3', '116 St & 85 Ave'),
+('Jonny55', 'Jonny', 'Wong', 'T2N1N4', '2500 University Dr'),
+('Ali67', 'Ali', 'Mo', 'N6A3K7', '1151 Richmond St'),
+('Dh78', 'Daniel', 'Humphrey', 'G1V0A6', '2325 Rue de l''Universite'),
+('Greg8', 'Greg', 'Donald', 'V5A1S6', '8888 University Dr'),
+('OldMac6', 'Ronald', 'MacDonald', 'M5S1A1', '27 King''s College Circle'),
+('Bigbird4', 'Tom', 'Tree', 'N2L3G1',  '200 University Ave W'),
+('Thomas4', 'Thomas', 'Singh', 'V6T1Z4', '2329 West Mall');
+
 CREATE TABLE Senior (
 	Username	VARCHAR(20),
 	PRIMARY KEY (Username),
@@ -51,7 +110,8 @@ VALUES
 (3, '2018-09-09', 'Take me to my Doctors appointment', 1, 'Completed', 'T6G2R3', '116 St & 85 Ave', 'Sarah123', '2018-08-09'),
 (4, '2018-07-17', 'Help me with learn how to use Facebook', 1, 'Completed', 'N6A3K7', '1151 Richmond St', 'Dorothy45', '2018-06-17'),
 (5, '2018-05-17', 'Help me mail a package back', 1, 'Completed', 'N6A3K7', '1151 Richmond St', 'Thomas4', '2018-04-17'),
-(6, '2018-05-19', 'Help me cut my cat''s nails', 1, 'ToDo', 'N6A3K7', '1151 Richmond St', 'Thomas4',  '2018-04-19');
+(6, '2020-05-19', 'Help me cut my cat''s nails', 1, 'Upcoming', 'N6A3K7', '1151 Richmond St', 'Thomas4',  '2020-04-19'),
+(7, '2020-05-20', 'Help me survive coronavirus', 10, 'Upcoming', 'N6A3K7', '1151 Richmond St', 'Thomas4',  '2020-04-19');
 
 CREATE TABLE Preference (
 	Pref_ID	    INTEGER,
@@ -116,6 +176,12 @@ CREATE TABLE VolunteerVolunteers (
 INSERT INTO  VolunteerVolunteers (Username, Task_ID, Date)
 VALUES
 ('Ann34', 1, '2020-01-31'),
+('Ann34', 2, '2019-12-31'),
+('Ann34', 3, '2018-09-09'),
+('Ann34', 4, '2018-07-17'),
+('Ann34', 5, '2018-05-17'),
+('Ann34', 6, '2020-05-19'),
+('Ann34', 7, '2020-05-20'),
 ('Jonny55', 2, '2019-12-31'),
 ('Ali67', 3, '2018-09-09'),
 ('Dh78', 4, '2018-07-17'),
@@ -137,6 +203,10 @@ CREATE TABLE VolunteersTimeEntryRecord (
 INSERT INTO VolunteersTimeEntryRecord (Record_ID, Date, TimeOfDay, Hours, Username, Task_ID)
 VALUES
 (123,'2020-01-31', '13:15', 3, 'Ann34', 1),
+(124,'2019-12-31', '14:00', 10, 'Ann34', 2),
+(125,'2018-09-09', '15:15', 2, 'Ann34', 3),
+(126,'2018-07-17', '16:30', 5, 'Ann34', 4),
+(127,'2018-05-17', '17:45', 20, 'Ann34', 5),
 (234,'2019-12-31', '14:50', 2, 'Jonny55', 2),
 (456,'2018-09-09', '9:30', 3, 'Ali67', 3),
 (567,'2018-07-17', '12:30', 2, 'Dh78', 4),
@@ -152,7 +222,12 @@ CREATE TABLE TaskRequestPlace (
 
 INSERT INTO TaskRequestPlace (Request_ID, Date, Task_ID)
 VALUES
-(6, '2018-06-17', 6);
+(612, '2018-06-17', 6),
+(613, '2018-06-17', 6),
+(614, '2018-06-17', 6),
+(712, '2020-05-18', 7),
+(713, '2020-05-19', 7),
+(714, '2020-05-19', 7);
 
 CREATE TABLE VolunteerPlaceRequest (
 	Username	VARCHAR(20),
@@ -165,7 +240,12 @@ CREATE TABLE VolunteerPlaceRequest (
 
 INSERT INTO VolunteerPlaceRequest (Request_ID, Username)
 VALUES
-(6, 'Greg8');
+(612, 'Greg8'),
+(613, 'Greg8'),
+(614, 'Greg8'),
+(712, 'Ali67'),
+(713, 'Dh78'),
+(714, 'Jonny55');
 
 CREATE TABLE TaskCompletePlaced (
 	Complete_ID		INTEGER,
@@ -182,7 +262,7 @@ VALUES
 (2, '2019-12-31', 2, 0),
 (3, '2018-09-09', 3, 0),
 (4, '2018-07-17', 4, 0),
-(5, '2018-05-17', 5, 0);
+(5, '2018-05-17', 5, 10);
 
 CREATE TABLE SeniorPlaceCompleteTask (
 	Username		VARCHAR(20),
@@ -218,6 +298,14 @@ INSERT INTO MakeReview (Review_ID, Description, Rating, Task_ID, VUsername)
 VALUES
 (10, 'Great Help! My lawn looks amazing!', 10, 1,  'Ann34'),
 (20, 'Not the best drive, a little too speedy for my liking', 7, 2, 'Jonny55'),
-(13, 'Made me feel very supported at the doctors and held the door', 10, 3, 'Ali67'),
+(103, 'Made me feel very supported at the doctors and held the door', 10, 3, 'Ali67'),
+(13, 'good support, but other volunteer was better.', 7, 3, 'Ann34'),
 (45, 'I know how to use facebook now well but they were a bit impatient', 8, 4, 'Dh78'),
-(11, 'My package got shipped, no complaints!', 9, 5, 'Greg8');
+(22, 'Ann did a very bad job. Do not recommend.', 1, 4, 'Ann34'),
+(11, 'My package got shipped, no complaints!', 9, 5, 'Greg8'),
+(29, 'My package got shipped, no complaints!', 9, 5, 'Ann34');
+
+
+
+
+
